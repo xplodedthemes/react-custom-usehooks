2ede01c128ec3b59b0728660aa59465198cd89f4
Georges Haddad <prismosoft@gmail.com>
2020-08-29 17:59:14 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import usePrevPropsAndState from "hooks/usePrevPropsAndState";
-import useGetSnapshotBeforeUpdate from "hooks/useGetSnapshotBeforeUpdate";
+import usePrevPropsAndState from "./hooks/usePrevPropsAndState";
+import useGetSnapshotBeforeUpdate from "./hooks/useGetSnapshotBeforeUpdate";
 
 export {
     usePrevPropsAndState,
