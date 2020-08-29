ada0fdf1487a98d0f9ff7bb59956192fff1db4ac
Georges Haddad <prismosoft@gmail.com>
2020-08-29 18:04:47 -0400

    Update

--- index.js
+++ index.js
@@ -18,13 +18,48 @@ function ___$insertStyle(css) {
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+var react = require('react');
 
-var usePrevPropsAndState = _interopDefault(require('hooks/usePrevPropsAndState'));
-var useGetSnapshotBeforeUpdate = _interopDefault(require('hooks/useGetSnapshotBeforeUpdate'));
+const usePrevPropsAndState = (props, state) => {
+    const prevPropsAndStateRef = react.useRef({ props: null, state: null });
+    const prevProps = prevPropsAndStateRef.current.props;
+    const prevState = prevPropsAndStateRef.current.state;
+
+    react.useEffect(() => {
+        prevPropsAndStateRef.current = { props, state };
+    });
+
+    return { prevProps, prevState }
+};
+
+const useGetSnapshotBeforeUpdate = (cb, props, state) => {
+    // get prev props and state
+    const { prevProps, prevState } = usePrevPropsAndState(props, state);
+
+    const snapshot = react.useRef(null);
 
 
+    // getSnapshotBeforeUpdate - not run on mount + run on every update
+    const componentJustMounted = react.useRef(true);
+    react.useLayoutEffect(() => {
+        if (!componentJustMounted.current) {
+            snapshot.current = cb(prevProps, prevState);
+        }
+        componentJustMounted.current = false;
+    });
 
-exports.usePrevPropsAndState = usePrevPropsAndState;
+    // 👇 look here
+    const useComponentDidUpdate = cb => {
+        react.useEffect(() => {
+            if (!componentJustMounted.current) {
+                cb(prevProps, prevState, snapshot.current);
+            }
+        });
+    };
+    // 👇 look here
+    return useComponentDidUpdate
+};
+
 exports.useGetSnapshotBeforeUpdate = useGetSnapshotBeforeUpdate;
+exports.usePrevPropsAndState = usePrevPropsAndState;
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-import usePrevPropsAndState from "./hooks/usePrevPropsAndState";
-import useGetSnapshotBeforeUpdate from "./hooks/useGetSnapshotBeforeUpdate";
+import {useEffect, useLayoutEffect, useRef} from "react";
 
-export {
-    usePrevPropsAndState,
-    useGetSnapshotBeforeUpdate
+export const usePrevPropsAndState = (props, state) => {
+    const prevPropsAndStateRef = useRef({ props: null, state: null })
+    const prevProps = prevPropsAndStateRef.current.props
+    const prevState = prevPropsAndStateRef.current.state
+
+    useEffect(() => {
+        prevPropsAndStateRef.current = { props, state }
+    })
+
+    return { prevProps, prevState }
 };
 
+export const useGetSnapshotBeforeUpdate = (cb, props, state) => {
+    // get prev props and state
+    const { prevProps, prevState } = usePrevPropsAndState(props, state)
+
+    const snapshot = useRef(null)
+
+
+    // getSnapshotBeforeUpdate - not run on mount + run on every update
+    const componentJustMounted = useRef(true)
+    useLayoutEffect(() => {
+        if (!componentJustMounted.current) {
+            snapshot.current = cb(prevProps, prevState)
+        }
+        componentJustMounted.current = false
+    })
+
+    // 👇 look here
+    const useComponentDidUpdate = cb => {
+        useEffect(() => {
+            if (!componentJustMounted.current) {
+                cb(prevProps, prevState, snapshot.current)
+            }
+        })
+    }
+    // 👇 look here
+    return useComponentDidUpdate
+};
